4747bebadf4eb72899407fd915ce11eaa9848f85
SkyGuyTheFlyGuy <skylertower@yahoo.com>
2023-11-16 11:46:49 -0700

    added homepage stuff

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -47,3 +47,18 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+// Page Visit Counter
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
