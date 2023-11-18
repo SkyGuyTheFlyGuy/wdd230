d5e2d3401a0ff1b371ddbd46211567ee45ccd716
SkyGuyTheFlyGuy <skylertower@yahoo.com>
2023-11-18 16:26:32 -0700

    fixed chamber

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -16,3 +16,13 @@ const lastModified = new Date(document.lastModified);
 const lastModifiedString = lastModified.toLocaleString();
 
 lastModifiedParagraph.textContent = lastModifiedString;
+
+// Hamburger
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
