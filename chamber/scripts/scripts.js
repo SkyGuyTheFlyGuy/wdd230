f51893c665537e1a96a54b5708dda9747325e1fe
SkyGuyTheFlyGuy <skylertower@yahoo.com>
2023-11-18 18:25:15 -0700

    added to chamber

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -26,3 +26,18 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+// Page Visit Counter
+
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
