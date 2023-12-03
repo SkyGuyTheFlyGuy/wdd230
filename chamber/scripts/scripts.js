26c75bcd1a30bad684264949c03810b3a8691b1e
SkyGuyTheFlyGuy <skylertower@yahoo.com>
2023-12-02 18:08:29 -0700

    added fix

--- chamber/scripts/scripts.js
+++ chamber/scripts/scripts.js
@@ -15,8 +15,11 @@ function showList() {
 	display.classList.remove("grid");
 }
 
-// Fetch JSON data
-fetch('members.json')
+// Define the URL of the JSON file
+const jsonUrl = 'https://skyguytheflyguy.github.io/wdd230/testpage/data/companies.json';
+
+// Fetch the JSON data from the defined URL
+fetch(jsonUrl)
   .then(response => response.json())
   .then(data => {
     const companyList = document.getElementById('company-list');
@@ -25,11 +28,11 @@ fetch('members.json')
     data.companies.forEach(company => {
       const companyDiv = document.createElement('div');
       companyDiv.innerHTML = `
-	    <img src="${company.image}" alt="${company.name} logo">
         <h2>${company.name}</h2>
         <p>Address: ${company.address}</p>
         <p>Phone: ${company.phone}</p>
         <p>Website: <a href="${company.website}" target="_blank">${company.website}</a></p>
+        <img src="${company.image}" alt="${company.name} logo">
         <p>Membership Level: ${company.membership_level}</p>
       `;
       companyList.appendChild(companyDiv);
